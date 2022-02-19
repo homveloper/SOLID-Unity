8badd88b3a6a77a4a7a773389fbf23c91b18842f
ToTo-Mo <bum4496@naver.com>
2022-02-19 13:11:30 +0900

    추가 : OOP 방식의 프로그래밍

---
+++ Assets/Examples/1-Traditional Programming/Scripts/Projectile.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Traditional
+{
+    public class Projectile : MonoBehaviour
+    {
+        private int damage;
+
+        public int Damage{get{return damage;}}
+    }
+
+}
+
--- Assets/Examples/1-Traditional Programming/Scripts/SpaceShip.cs
+++ Assets/Examples/1-Traditional Programming/Scripts/SpaceShip.cs
@@ -2,17 +2,91 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpaceShip : MonoBehaviour
+namespace Traditional
 {
-    // Start is called before the first frame update
-    void Start()
+    public class SpaceShip : MonoBehaviour
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [SerializeField] private int health = 100;
+        [SerializeField] private int maxHealth = 100;
+
+        [SerializeField] private float moveSpeed;
+        [SerializeField] private float turnSpeed;
+        [SerializeField] private float fireForce;
+        [SerializeField] private Rigidbody2D projectile;
+        [SerializeField] private Transform weaponMountPosition;
+        [SerializeField] private GameObject thrusterParticles;
+
+
+        public int Health
+        {
+            get
+            {
+                return health;
+            }
+            set
+            {
+                health += value;
+
+                if (health <= 0)
+                {
+                    Die();
+                }
+            }
+        }
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            health = maxHealth;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            FireWeapon();
+            Move();
+        }
+
+        private void Die()
+        {
+            Destroy(gameObject);
+        }
+
+        private void Move()
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            transform.position += transform.up * vertical * moveSpeed * Time.deltaTime;
+            transform.Rotate(-Vector3.forward * horizontal * turnSpeed * Time.deltaTime);
+        }
+
+        private void FireWeapon()
+        {
+            if (Input.GetButtonDown("PrimaryFire"))
+            {
+                Rigidbody2D spawnedProjectile = Instantiate(projectile, weaponMountPosition.position, weaponMountPosition.rotation);
+                spawnedProjectile.AddForce(spawnedProjectile.transform.up * fireForce);
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            Projectile projectile;
+
+            if (other.collider.TryGetComponent<Projectile>(out projectile))
+            {
+                Debug.Log(projectile);
+                TakeDamage(projectile.Damage);
+                Destroy(projectile.gameObject);
+            }
+        }
+
+        public void TakeDamage(int value)
+        {
+            Health -= value;
+        }
     }
 }
